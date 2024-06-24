fa5090a69f2b0586128e4332d33cd23330edf769
kleryxx <126974469+Vlad-Lipinskyi@users.noreply.github.com>
2024-06-24 11:23:54 +0300

    sss

--- index.js
+++ index.js
@@ -26,3 +26,45 @@
 
 // Завдання 4
 
+// const numbers = [1, 2, 3, 4, 5]; 
+// let number = 0; 
+
+// while (number < numbers.length) {
+//     console.log(numbers[number]); 
+//     number++; 
+// }
+
+// Завдання 5
+
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; 
+
+// for (const number of numbers) {
+//     if (number === 7) {
+//         break; 
+//     }
+//     console.log(number); // вивести число
+// }
+
+
+// Завдання 6
+
+// const n = Number(prompt("Введіть число"));
+// let i = 0;
+// while(i < n){
+//     console.log(i);
+//     i++;
+// }
+
+
+// Завдання 7
+
+let number = 1;
+
+while (number <= 20) {
+    if (number % 3 === 0) {
+        number += 1;
+        continue; 
+    }
+    console.log(number); 
+    number += 1;
+}
